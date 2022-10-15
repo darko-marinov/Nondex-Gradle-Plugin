e250ee0324357ed3bd1bce193763970bb336b7c7
Justin Chen <jjchen10@illinois.edu>
2022-10-15 17:07:51 -0500

    Add Nondex help task

--- plugin/src/main/java/edu/illinois/nondex/gradle/plugin/NondexGradlePlugin.java
+++ plugin/src/main/java/edu/illinois/nondex/gradle/plugin/NondexGradlePlugin.java
@@ -1,6 +1,7 @@
 package edu.illinois.nondex.gradle.plugin;
 
 import edu.illinois.nondex.gradle.tasks.NondexClean;
+import edu.illinois.nondex.gradle.tasks.NondexHelp;
 import edu.illinois.nondex.gradle.tasks.NondexTest;
 import org.gradle.api.Project;
 import org.gradle.api.Plugin;
@@ -9,6 +10,7 @@ public class NondexGradlePlugin implements Plugin<Project> {
     public void apply(Project project) {
         project.getTasks().create(NondexTest.getNAME(), NondexTest.class).init();
         project.getTasks().create(NondexClean.getNAME(), NondexClean.class).init();
+        project.getTasks().create(NondexHelp.getNAME(), NondexHelp.class).init();
     }
 }
 
