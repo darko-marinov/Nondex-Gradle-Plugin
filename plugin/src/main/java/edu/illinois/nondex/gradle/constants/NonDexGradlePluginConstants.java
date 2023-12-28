4046ef8735deb8ac7466bb9f459e3b68972557d3
Kaiyao Ke <kaiyaok2@illinois.edu>
2023-12-28 16:25:04 +0800

    update to version 2.1.7

--- plugin/src/main/java/edu/illinois/nondex/gradle/constants/NonDexGradlePluginConstants.java
+++ plugin/src/main/java/edu/illinois/nondex/gradle/constants/NonDexGradlePluginConstants.java
@@ -2,6 +2,6 @@ package edu.illinois.nondex.gradle.constants;
 
 public abstract class NonDexGradlePluginConstants {
 
-    public static final String NONDEX_VERSION = "2.1.1";
-    public static final String NONDEX_COMMON_SHA1 = "4b0ca8ba572baa78d15f8a1f3ecf2bb918cbe9b1";
+    public static final String NONDEX_VERSION = "2.1.7";
+    public static final String NONDEX_COMMON_SHA1 = "70dd8887de7a6c46863eca52f1809ed73274d90e";
 }
